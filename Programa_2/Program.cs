0e3b83e1c47193e1ec122225a39d3e48efabf85a
Lorenzo Schauenberg <lorenzoschauenberg@gmail.com>
2023-08-24 15:16:20 -0300

    teste de commit no visual studio

--- Programa_2/Program.cs
+++ Programa_2/Program.cs
@@ -10,11 +10,23 @@ namespace Projeto_1
         {
             int idade = 19;
             string nome = "lorenzo";
-            double saldo = 5000.34123;
+            double saldo = 1000.34123;
 
             Console.WriteLine("Meu nome é " + nome + ", eu tenho " + idade + " anos e meu saldo é " + saldo.ToString("F2")); // Concatenação!
             Console.WriteLine("Meu nome é {0}, eu tenho {1} anos e meu saldo é {2:F2}", nome, idade, saldo); // Placeholders!
             Console.WriteLine($"{nome} tem {idade} e {saldo:F2} na conta"); // Interpolação!
-            }
+
+            double a = 5.15;
+            int b = 1;
+
+            b = (int)a;
+            Console.WriteLine(b);
+
+            a = a / b;
+            Console.WriteLine(a);
+
+            Console.WriteLine(Math.Pow(2, 10));
+            Console.WriteLine(Math.Sqrt(32));
+        }
     }
 }
