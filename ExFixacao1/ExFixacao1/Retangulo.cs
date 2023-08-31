65f8b2c8945797de12c0d0b4bf6e3127e1cb8274
Lorenzo Schauenberg <lorenzoschauenberg@gmail.com>
2023-08-31 17:40:34 -0300

    Estudos de C#

---
+++ ExFixacao1/ExFixacao1/Retangulo.cs
@@ -0,0 +1,21 @@
+﻿namespace ExFixacao1
+{
+    internal class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+        public double Area(double L, double A)
+        {
+            return L * A;
+        }
+        public double Perimetro(double L, double A)
+        {
+            return (L + A) * 2;
+        }
+        public double Diagonal(double L, double A)
+        {
+            return Math.Sqrt(Math.Pow(L, 2) + Math.Pow(A, 2));
+        }
+    }
+}
---
+++ ExFixacao2/ExFixacao2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExFixacao2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f = new Funcionario();
+            Console.WriteLine("Nome: ");
+            f.Nome = Console.ReadLine();
+            Console.WriteLine("Salário bruto: ");
+            f.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Imposto: ");
+            f.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Funcionário: " + f);
+
+            Console.Write("Deseja aumentar o salário em qual porcentagem? ");
+            double Porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            f.AumentarSalario(Porcentagem);
+
+            Console.Write("Dados atualizados: " + f);
+        }
+    }
+}
