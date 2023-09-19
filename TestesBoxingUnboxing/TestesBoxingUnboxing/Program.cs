f9d1cf7b435b1635cde6acd3e675602f0433d957
Lorenzo Schauenberg <lorenzoschauenberg@gmail.com>
2023-09-19 18:16:36 -0300

    Add files via upload

---
+++ TestesBoxingUnboxing/TestesBoxingUnboxing/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace TestesBoxingUnboxingForeach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 20;
+            Object obj = x; //boxing
+            Console.WriteLine(obj);
+
+            int y = (int)obj; //unboxing
+            Console.WriteLine(y);
+
+            string[] vect = new string[] {"AAAAA", "BBBBB", "CCCCC"};
+
+            foreach (string ve in vect) // para cada "ve" contido em "vect"
+            {
+                Console.Write(ve + " ");
+            }
+        }
+    }
+}
