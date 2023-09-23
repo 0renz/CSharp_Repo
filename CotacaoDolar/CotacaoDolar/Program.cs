2a1e8a7fbcc3ac4c65df74ebfcd2bfe8b7005522
Lorenzo <lorenzoschauenberg@gmail.com>
2023-09-23 11:38:34 -0300

    vai da bom

---
+++ CotacaoDolar/CotacaoDolar/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace CotacaoDolar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual é a cotação do dolar?");
+            double CotDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Quantos dólares você vai comprar?");
+            double Dolares = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double valorConvertido = ConversorDeMoeda.converteMoeda(CotDolar, Dolares);
+            Console.WriteLine("Valor a ser pago em reais: " + valorConvertido.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
