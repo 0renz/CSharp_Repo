1fc8997cdb5cdef616cf976c63feeba15b575798
Lorenzo <lorenzoschauenberg@gmail.com>
2023-11-21 18:19:52 -0300

    Testes com enums

---
+++ TesteEnums/TesteEnums/TesteEnums/Program.cs
@@ -0,0 +1,24 @@
+﻿using TesteEnums.Entities;
+using TesteEnums.Entities.Enums;
+
+namespace TesteEnums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            OrderStatus order2 = Enum.Parse<OrderStatus>("Delivered");
+            Console.WriteLine(order2);
+        }
+    }
+}
+
